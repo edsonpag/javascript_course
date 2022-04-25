bc7f029746cf60bf87000f4d017f7d2967b0e815
edsonpag <edsonsilvapagani@gmail.com>
2022-04-25 20:41:23 -0300

    start btn

--- Card Memory/app.js
+++ Card Memory/app.js
@@ -52,28 +52,31 @@ app()
 
 const randomSequenceOfNumbers = getRandomSequenceOfNumbers()
 const cardsEl = document.querySelectorAll('.card')
-
 let selectedCards = []
 let selectedCardsId = []
 
-cardsEl.forEach((card, index) => {
-    card.addEventListener('click', async (event) => {
-        const selectedCard = randomSequenceOfNumbers[index]
+const startBtn = document.querySelector('button')
 
-        card.innerHTML = `
-            <img src=${cards[selectedCard]} class="img-card" data-card-id=${selectedCard}>
-        `
-
-        selectedCards.push(card)
-        selectedCardsId.push(selectedCard)
-        
-        if(selectedCards.length === 2) {
-            await setTimeout(async () => {
-                await hiddenCards()
-            }, 50)
+startBtn.addEventListener('click', (event) => {
+    cardsEl.forEach((card, index) => {
+        card.addEventListener('click', async (event) => {
+            const selectedCard = randomSequenceOfNumbers[index]
+    
+            card.innerHTML = `
+                <img src=${cards[selectedCard]} class="img-card" data-card-id=${selectedCard}>
+            `
+    
+            selectedCards.push(card)
+            selectedCardsId.push(selectedCard)
             
-        }
-        
+            if(selectedCards.length === 2) {
+                await setTimeout(async () => {
+                    await hiddenCards()
+                }, 50)
+                
+            }
+            
+        })
     })
 })
 
@@ -87,3 +90,9 @@ function hiddenCards() {
     selectedCards = []
     selectedCardsId = []
 }
+
+// adicionar o timer
+// quando o usuario finalizar o jogo mostrar uma mensagem de parabens com o contador exibindo o tempo que ele levou
+// clicando em ok o jogador inicia um novo game
+// Adicionar niveis de dificuldade (facil, medio e hard). Quanto maior a dificuldade, menos tempo ele tem para completar e aumentar o número de cards
+// O usuario pode ver as estaticas do jogo: número de vezes que ganhou/perdeu e o melhor tempo para cada nivel, armazenar no localStorage caso o usuario recarregue a pagina
