5a4f5bfc34a68c11b6928a423e8d0d0561d41789
edsonpag <edsonsilvapagani@gmail.com>
2022-04-26 08:26:07 -0300

    add: timer, bug fix: quando clicava em uma carta que ja estava face up agora não vira mais ela para facedown. Add: Game win message

--- Card Memory/app.js
+++ Card Memory/app.js
@@ -1,5 +1,6 @@
 const cards_length = 6
 const cards = []
+let allCardsShown = 0
 
 const mainEl = document.querySelector('main')
 
@@ -58,8 +59,12 @@ let selectedCardsId = []
 const startBtn = document.querySelector('button')
 
 startBtn.addEventListener('click', (event) => {
+    timer()
     cardsEl.forEach((card, index) => {
         card.addEventListener('click', async (event) => {
+
+            if(card.firstElementChild.getAttribute('data-card-id')) return
+
             const selectedCard = randomSequenceOfNumbers[index]
     
             card.innerHTML = `
@@ -75,7 +80,6 @@ startBtn.addEventListener('click', (event) => {
                 }, 50)
                 
             }
-            
         })
     })
 })
@@ -85,14 +89,51 @@ function hiddenCards() {
         for(i of selectedCards) {
             i.innerHTML = `<img src="./cards/facedown.jpg" class="img-card">`
         }
+    } else {
+        allCardsShown++
+        console.log(allCardsShown)
+    }
+
+    if(isGameOver()) {
+        window.alert('Você ganhou com o tempo de ' + timerEl.innerHTML)
     }
 
     selectedCards = []
     selectedCardsId = []
 }
 
-// adicionar o timer
-// quando o usuario finalizar o jogo mostrar uma mensagem de parabens com o contador exibindo o tempo que ele levou
+const timerEl = document.querySelector('.timer')
+
+function timer() {
+    let minutes = 0, seconds = 0
+
+    const time = setInterval(() => {
+        seconds++
+
+        if(seconds < 10) {
+            timerEl.innerHTML = `0${minutes}:0${seconds}`
+        } else {
+            timerEl.innerHTML = `0${minutes}:${seconds}`
+        }
+        
+        if(seconds === 60) {
+            minutes++
+            seconds = 0
+        }
+
+    }, 1000)
+}
+
+
+function isGameOver() {
+    if(allCardsShown === cards.length) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
 // clicando em ok o jogador inicia um novo game
 // Adicionar niveis de dificuldade (facil, medio e hard). Quanto maior a dificuldade, menos tempo ele tem para completar e aumentar o número de cards
 // O usuario pode ver as estaticas do jogo: número de vezes que ganhou/perdeu e o melhor tempo para cada nivel, armazenar no localStorage caso o usuario recarregue a pagina
